bd7aba2477e7e61bcc9954034a9cd7bd4dfce47c
Harshvardhan1806314 <harshvardhansi@infinite.com>
2025-01-08 21:54:22 +0530

    added models for Role and UserRole Table

---
+++ RESTApi/RESTApi/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTApi.Models
+{
+    public class Role
+    {
+        [Key]
+        public string RoleId { get; set; }
+
+        [Required(ErrorMessage = "Role Name cannot be empty")]
+        public string Name { get; set; }
+    }
+}
---
+++ RESTApi/RESTApi/Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTApi.Models
+{
+    public class UserRole
+    {
+        [Key]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Role Id cannot be empty")]
+        public string RoleId { get; set; }
+    }
+}
