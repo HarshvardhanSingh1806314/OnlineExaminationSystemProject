d6ee478ed2814d1cdb2567b547e148091fc110d9
Harshvardhan1806314 <harshvardhansi@infinite.com>
2025-01-09 00:58:28 +0530

    add migration for changing graduation year data type from string to int

---
+++ RESTApi/RESTApi/Migrations/202501081658532_changedDataTypeOfGraduationYearFromStringToInt.cs
@@ -0,0 +1,18 @@
+﻿namespace RESTApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changedDataTypeOfGraduationYearFromStringToInt : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Students", "GraduationYear", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Students", "GraduationYear", c => c.String(nullable: false));
+        }
+    }
+}
--- RESTApi/RESTApi/Migrations/Configuration.cs
+++ RESTApi/RESTApi/Migrations/Configuration.cs
@@ -1,10 +1,7 @@
 ﻿namespace RESTApi.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using RESTApi.Models;
-    using System.Linq;
     using RESTApi.Utility;
 
     internal sealed class Configuration : DbMigrationsConfiguration<RESTApi.DataAccess.ApplicationContext>
