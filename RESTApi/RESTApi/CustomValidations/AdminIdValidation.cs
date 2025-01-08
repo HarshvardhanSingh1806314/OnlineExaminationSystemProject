54778164cfd9dc5f03754184e48ce2697be19155
Harshvardhan1806314 <harshvardhansi@infinite.com>
2025-01-09 00:57:47 +0530

    added custom validation for adminId and graduationYear

---
+++ RESTApi/RESTApi/CustomValidations/AdminIdValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RESTApi.CustomValidations
+{
+    public class AdminIdValidation : ValidationAttribute
+    {
+        // Minimum value allowed for Admin Id
+        private const int MinimumAllowedValueForAdminId = 100000;
+
+        public AdminIdValidation(string ErrorMessage) : base(ErrorMessage)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            if(value == null)
+            {
+                return false;
+            }
+
+            int adminId = Convert.ToInt32(value.ToString());
+
+            // checking if adminId is valid
+            return adminId >= MinimumAllowedValueForAdminId;
+        }
+    }
+}
---
+++ RESTApi/RESTApi/CustomValidations/GraduationYearValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RESTApi.CustomValidations
+{
+    public class GraduationYearValidation : ValidationAttribute
+    {
+        // Minimum acceptable value for graduation year
+        private const int MinimumGraduationYearAllowed = 1970;
+
+        public GraduationYearValidation(string ErrorMessage) : base(ErrorMessage)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            if(value == null)
+            {
+                return false;
+            }
+
+            int graduationYear = Convert.ToInt32(value.ToString());
+
+            // checking if the graduation year is valid
+            return graduationYear >= MinimumGraduationYearAllowed;
+        }
+    }
+}
