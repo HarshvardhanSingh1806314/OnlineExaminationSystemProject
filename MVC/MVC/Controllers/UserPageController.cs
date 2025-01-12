336ffd5abce74f4087a95ed65765410aeffd10b7
Saikeerthi Tummagunta <saikeerthit@infinite.com>
2025-01-12 18:18:44 +0530

    admin

---
+++ MVC/MVC/Controllers/UserPageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class UserPageController : Controller
+    {
+        // GET: UserPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Report()
+        {
+            ViewBag.Message = "Your User page.";
+            return View();
+        }
+        public ActionResult Exam()
+        {
+            ViewBag.Message = "Your User page.";
+            return View();
+        }
+    }
+}
